4dc218c3db5f227e9ae1b920264167ff9876ca7a
valentin12345678910 <hengliang.shen@hotmail.com>
2025-12-07 16:43:01 +0100

    tzuioüpi9jkiöp0jiohojiku909iuhjuiopolkjui9o0plk

--- plattformer.py
+++ plattformer.py
@@ -36,7 +36,6 @@ class GameView(arcade.Window):
         self.game_over = False
         self.game_won = False
 
-        self.physik.enable_multi_jump(2)
 
     
 
@@ -128,6 +127,15 @@ class GameView(arcade.Window):
             self.game_over = random.choice([True, False])
             if not self.game_over:
                 self.time_left += 50
+
+
+
+        if self.game_over:
+            arcade.draw_text("Game Over! Drücke Leertaste zum Neustarten.", self.width / 2, self.height / 2,
+                             arcade.color.RED, 24, anchor_x="center")
+        elif self.game_won:
+            arcade.draw_text("Du hast gewonnen! Drücke Leertaste zum Neustarten.", self.width / 2, self.height / 2,
+                             arcade.color.GREEN, 24, anchor_x="center")
           
 
         
